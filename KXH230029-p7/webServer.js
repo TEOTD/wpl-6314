23c18190440e36c95c552095fe9a57edd6a7f152
mrfrozen97 <mrfrozenpeak@gmail.com>
2024-11-13 17:08:01 -0600

    Adding upload images

--- KXH230029-p7/webServer.js
+++ KXH230029-p7/webServer.js
@@ -40,6 +40,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const multer = require("multer");
+const fs = require("fs");
+const processFormBody = multer({storage: multer.memoryStorage()}).single('uploadedphoto');
+
 // Load the Mongoose schema for User, Photo, and SchemaInfo
 const {Types} = require("mongoose");
 const User = require("./schema/user.js");
@@ -169,13 +173,64 @@ app.get("/user/:id", async function (request, response) {
     }
 });
 
+
+/**
+ * URL /photos/new - Acccepts an image file in the body.
+ */
+app.post('/photos/new', (req, res) => {
+    processFormBody(req, res, (err) => {
+      if (err || !req.file) {
+        console.error('File upload error:', err);
+        return res.status(400).send({ error: 'File upload failed' });
+      }
+  
+      // Validate file type and size
+      const allowedMimeTypes = ['image/jpeg', 'image/png'];
+      if (!allowedMimeTypes.includes(req.file.mimetype)) {
+        return res.status(400).send({ error: 'Invalid file type' });
+      }
+  
+      const timestamp = Date.now();
+      const fileName = `U${timestamp}_${req.file.originalname}`;
+      const filePath = './images/'+ fileName;
+  
+      // Write the file to the "images" directory
+      fs.writeFile(filePath, req.file.buffer, (err1) => {
+        if (err1) {
+          console.error('Error saving file:', err1);
+          return res.status(500).send({ error: 'File save failed' });
+        }
+
+        try{
+            const image = {
+                file_name: fileName,
+                user_id: "6734eaa689fda133f5b3d85a",
+                date_time: timestamp,
+                comments: [],
+            };
+            Photo.create(image);
+            return res.status(200).json({ message: 'File uploaded successfully', fileName });
+        } catch(error1){
+            return res.status(400).json({ error: 'Could not save image to the table.' });
+        }
+  
+      });
+
+      return;
+    });
+});
+  
+  
+/**
+ * URL /commentsOfPhoto/:photo_id - Acccepts an comment in the body and adds it to the database.
+ */
 app.post("/commentsOfPhoto/:photo_id",  async (req, res) => {
     const photo_id = req.params.photo_id;
     const comment = req.body.comment;
     try{
         const newComment = {
             comment: comment,
-            user_id: "6722ed1829d9cf37406c5f20",
+            user_id: "6734eaa689fda133f5b3d85a",
             date_time: new Date(),
         };
         const result = await Photo.findByIdAndUpdate(
