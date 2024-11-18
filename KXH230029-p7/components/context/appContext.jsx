ac5035a52ad1c9c30a311212d4b54a3fae6928ed
mrfrozen97 <mrfrozenpeak@gmail.com>
2024-11-18 13:41:58 -0600

    Removing content default function

--- KXH230029-p7/components/context/appContext.jsx
+++ KXH230029-p7/components/context/appContext.jsx
@@ -1,18 +1,14 @@
 import {createContext} from "react";
 
 // Creating context to select advanced features, for login and reloading on input of comments.
-// eslint-disable-next-line no-unused-vars
-export const AdvancedContext = createContext([true, p => {
-}]);
-// eslint-disable-next-line no-unused-vars
-export const LoginContext = createContext([true, b => {
-}]);
-// eslint-disable-next-line no-unused-vars
-export const LoggedInUserContext = createContext([null, b => {
-}]);
-// eslint-disable-next-line no-unused-vars
-export const ReloadContext = createContext([true, p => {
-}]);
-// eslint-disable-next-line no-unused-vars
-export const FirstLoadContext = createContext([true, p => {
-}]);
+// Context used to check if the advanced options is enables
+export const AdvancedContext = createContext([true,]);
+// Used as a login flag
+export const LoginContext = createContext([true,]);
+// Used to get the logged in user
+export const LoggedInUserContext = createContext([null,]);
+// Context used to reload cound of comments and photos as well as
+// reload photos page on photo upload.
+export const ReloadContext = createContext([true,]);
+// Context used to handle opening of bookmark links.
+export const FirstLoadContext = createContext([true,]);
