1138917ead71cc82de225bb8743308e5fa009676
Kiran Hegde <kiransrivani@gmail.com>
2024-11-19 17:57:02 -0600

    first load bug fix

--- KXH230029-p7/photoShare.jsx
+++ KXH230029-p7/photoShare.jsx
@@ -209,6 +209,7 @@ function App() {
                     });
             })();
         } else {
+            setFirstLoad(false);
             navigate("/admin/login", {replace: true});
         }
     }, []);
